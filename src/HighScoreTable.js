0102874b887a4ce18f8791582982f6456aefef39
Oksana Kostenko <kseniavlad31@gmail.com>
2021-04-19 20:52:36 +0100

    creates components skeleton

---
+++ src/HighScoreTable.js
@@ -0,0 +1,14 @@
+/* eslint-disable indent */
+import React from "react";
+import PlayerScore from "./PlayerScore.js";
+
+const HighScoreTable = () => {
+  return (
+    <div>
+      <h1>Table</h1>
+      <PlayerScore />
+    </div>
+  );
+};
+
+export default HighScoreTable;
---
+++ src/PlayerScore.js
@@ -0,0 +1,8 @@
+/* eslint-disable indent */
+import React from "react";
+
+const PlayerScore = () => {
+  return <div>Score</div>;
+};
+
+export default PlayerScore;
